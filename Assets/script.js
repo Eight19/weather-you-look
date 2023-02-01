9d004d621fcdc9d22302514260384224a9b0e66d
Tiffany Jones <eight19realty@gmail.com>
2023-01-31 19:38:22 -0500

    fixed api key error

--- Assets/script.js
+++ Assets/script.js
@@ -1,24 +1,25 @@
-/*Added variables to pull details from OpenAPI*/
-var searchText = document.querySelector(".searchWeather");
-var city = document.querySelector(".weatherLocation");
-var day = document.querySelector(".dayWeather");
+var search = document.querySelector(".searchWeather");
+var city = document.querySelector(".weatherCity");
+var day = document.querySelector(".weatherDay");
 var humidity = document.querySelector(".weather_detail--humidity>.value");
 var precipitation = document.querySelector(".weather_detail--precipitation>.value");
 var wind = document.querySelector(".weather_detail--wind>.value");
 var photo = document.querySelector(".weatherPhoto");
-var temp = document.querySelector(".temperature>.value");
-/*Fetched city weather info using OpenWeatherAPI*/
-var weatherApiKey = "92ca0200c0393370e2aa006ad308d849";
-var weatherBaseEndpoint = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
+var temperature = document.querySelector(".weatherTemp>.value");
+var weatherApiKey = "ba180dbe78d3d4d9710851c9e3f85643";
+var weatherBaseEndpoint = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=ba180dbe78d3d4d9710851c9e3f85643" 
+
+
 var getWeatherByCityName = async (city) => {
   var endpoint = weatherBaseEndpoint + "&q=" + city;
   var response = await fetch(endpoint);
   var weather = await response.json();
-  console.log (weather);
+
+  console.log(weather);
 }
-/*Search bar action*/
-searchText.addEventListener("keydown", (e) => {
-  if(e.keyCode ===13) {
-    var weather = getWeatherByCityName(searchText.value);
+
+search.addEventListener("keydown" , (e) => {
+  if(e.keyCode === 13) {
+      let weather = getWeatherByCityName(search.value);
   }
 })
